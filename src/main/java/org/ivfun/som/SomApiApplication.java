007ba4db1526dc6873f6363320c027e861a69b2c
jonathan <jonathan.henrique.smtp@gmail.com>
2018-07-17 19:20:25 -0300

    init

---
+++ src/main/java/org/ivfun/som/SomApiApplication.java
@@ -0,0 +1,14 @@
+package org.ivfun.som;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomApiApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SomApiApplication.class, args);
+    }
+}
